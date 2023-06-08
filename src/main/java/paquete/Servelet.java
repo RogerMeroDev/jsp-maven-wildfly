4d3cc36b5478e0d3ee0ee8a47e4fb09d1977c4d9
RogerMero <rog3r.m3ro@gmail.com>
2023-06-07 22:44:45 -0500

    first commit

---
+++ src/main/java/paquete/Servelet.java
@@ -0,0 +1,32 @@
+package paquete;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Servelet")
+public class Servelet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+    public Servelet() {
+        super();
+      
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().println("<html><body><h1>Hola Mundo</h1></body></html>");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
